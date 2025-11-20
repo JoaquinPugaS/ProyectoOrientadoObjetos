838424c7301849e9608ece7edcbbf98902b7834a
Mati-U <matiasugarte1718@gmail.com>
2025-11-19 21:55:48 -0300

    Agrega main a conexion para verificar

---
+++ src/DataBase/Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Puga
+ */
+public class Conexion {
+    
+    public static Connection obtenerConexion (){
+        
+        Connection conexion = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/libreria", "root", "");
+            
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return conexion;
+    }
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        con.obtenerConexion();
+    }
+}
