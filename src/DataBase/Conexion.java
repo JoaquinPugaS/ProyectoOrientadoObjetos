154deb9d140673080a484a482c8553986c19b650
Mati-U <matiasugarte1718@gmail.com>
2025-11-19 22:01:15 -0300

    Agrega main a conexion para verificar

--- src/DataBase/Conexion.java
+++ src/DataBase/Conexion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DataBase;
 
 import java.sql.Connection;
@@ -33,4 +29,5 @@ public class Conexion {
         Conexion con = new Conexion();
         con.obtenerConexion();
     }
+
 }
